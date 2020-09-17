11163e54eb5127b55e91cadd8869c8b3a875f6d4
Cristobal Heiss <barbacoabq@gmail.com>
2020-09-17 17:46:08 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  correct = false;
   title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  checkLetter(enteredChar: string, randomChar: string) {
+    console.log('enteredChar: ', enteredChar, '. randomChar: ', randomChar);
+    if (enteredChar === undefined) {
+      return 'notEntered';
+    } else if (enteredChar === randomChar) {
+      return 'correct';
+    } else {
+      return 'incorrect';
+    }
+  }
+
+  onChangeText(value: string) {
+    this.enteredText = value;
+  }
 }
